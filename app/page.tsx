9db610e0e239f14303899f478484f8e4503dcde8
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2023-10-24 18:33:38 +0600

    Create SearchBar component. Create SearchManufacturer component and SearchManufacturerProps interface. Add custom styles for SearchBar and SearchManufacturer components. Add HeadlessUI as dependency. Add list of car manufacturer names to constants.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { Hero } from '@/components'
-import Image from 'next/image'
+import { Hero, SearchBar, CustomFilter } from '@/components';
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="overflow-hidden">
-      <Hero/>
+    <main>
+      <Hero />
+      <div id="discover" className="mt-12 max-width padding-x padding-y">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore Cars</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="{fuel} " />
+            <CustomFilter title="{gear}" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
